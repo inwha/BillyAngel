b566e790214f5a2c36da82aa3b45c88adf8affe3
kim.inwha <kim.inwha@navercorp.com>
2020-02-15 18:17:26 +0900

    settingFragment 중복으로 나타나는 버그 수

--- app/src/main/java/com/billyangel/MainActivity.kt
+++ app/src/main/java/com/billyangel/MainActivity.kt
@@ -3,8 +3,10 @@ package com.billyangel
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.billyangel.setting.SettingFragment
 import kotlinx.android.synthetic.main.main_activity.*
 import kotlinx.android.synthetic.main.view_title.*
 
@@ -25,9 +27,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun titleBar(navController: NavController) {
         settingBtn.setOnClickListener {
-            navController.navigate(R.id.settingFragment)
+            val currentFragment = getCurrentFragment()
+            if (currentFragment != null && currentFragment is SettingFragment) {
+                return@setOnClickListener
+            }
+            val navOption = makeRightNavAnim()
+
+            navController.navigate(R.id.settingFragment, null, navOption)
         }
     }
+
+    private fun makeRightNavAnim() = NavOptions.Builder()
+        .setEnterAnim(R.anim.slide_in_right)
+        .setPopExitAnim(R.anim.slide_out_right)
+        .build()
+
+    private fun getCurrentFragment() =
+        supportFragmentManager.findFragmentById(R.id.hostLayout)?.let {
+            it.childFragmentManager.fragments[0]
+        }
 
     private fun initBottomView(navController: NavController) {
         bottomView.setupWithNavController(navController)
